199cbba0325db55904b879ddb6e5679453458e6c
Denis Baturin <ndbaturin@gmail.com>
2024-02-11 20:47:07 +0400

    test deploy task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,47 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const MNEMONIC = process.env.MNEMONIC;
+const ACCOUNTS = MNEMONIC ? { "mnemonic": MNEMONIC, "initialIndex": 0 } : "remote";
+
+task("test-deploy", "Test deployment")
+  .setAction(async (taskArgs) => {
+    await hre.run("compile");
+
+    const TestERC20 = await ethers.getContractFactory("TestERC20");
+    const paymentToken = await TestERC20.deploy();
+    const paymentTokenAddress = await paymentToken.getAddress();
+
+    console.log("TestERC20 deployed to ", paymentTokenAddress);
+
+    const price = 10*18;
+    const accounts = await hre.ethers.getSigners();
+
+    const LifeHackatonItems = await ethers.getContractFactory("LifeHackatonItems");
+    const lifeHackatonItems = await upgrades.deployProxy(
+      LifeHackatonItems,
+      ["https://test.uri", paymentTokenAddress, paymentTokenAddress, price, price, accounts[0].address]
+    );
+    await lifeHackatonItems.waitForDeployment();
+
+    console.log("LifeHackatonItems deployed to ", await lifeHackatonItems.getAddress());
+  });
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    op_bnb_test: {
+      url: process.env.POLYGON_ZKEVM_URL || "https://opbnb-testnet-rpc.bnbchain.org/",
+      accounts: ACCOUNTS
+    }
+  },
 };
